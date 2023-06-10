bae3318435a5d212cf11043046a00206e70f4af3
linzy <875755045@qq.com>
2023-06-10 09:43:45 +0800

    basic config

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+    <div @click="theme = 'light'">浅色</div>
+    <div @click="theme = 'dark'">深色</div>
+  </div>
+</template>
+<script setup lang="ts">
+import useTheme from '@/hooks/useTheme.ts'
+
+const { theme } = useTheme()
+</script>
+<style lang="scss" scoped>
+.test {
+  width: 100%;
+  height: 100%;
+  @include useTheme() {
+    background-color: getVar('bgColor');
+    color: getVar('textColor');
+  }
+}
+</style>
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,7 @@
-const { Configuration } = require('webpack')
+const webpack = require('webpack')
+const { Configuration } = webpack
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 const { resolve } = require('path')
 
 /**
@@ -7,15 +9,44 @@ const { resolve } = require('path')
  */
 module.exports = {
   mode: 'development',
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     filename: '[name].js',
     path: resolve(__dirname, '../dist')
   },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '../src'),
+    },
+  },
   module: {
     rules: [
+      // 处理 .sass/scss 文件
       {
-        test: /\.m?js$/,
+        test: /\.s[ac]ss$/,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          {
+            loader: 'sass-loader',
+            // 配置全局可用的scss文件
+            options: {
+              additionalData: `@import "@/assets/styles/theme.scss";`
+            }
+          }
+        ],
+      },
+      // 处理.vue 文件
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      // 处理.ts 文件
+      {
+        test: /\.ts$/,
         exclude: /(node_modules)/,
         use: {
           // `.swcrc` can be used to configure swc
@@ -25,10 +56,21 @@ module.exports = {
     ]
   },
   plugins: [
+    // 处理html文件
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    // 处理.vue文件
+    new VueLoaderPlugin(),
+    // 处理控制台 Warning
+    new webpack.DefinePlugin({
+      // 禁用Vue Options API
+      __VUE_OPTIONS_API__: true,
+      // 禁用生产环境 DevTools
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
   ],
+  // 本地服务
   devServer: {
     hot: true,
     host: '0.0.0.0',
